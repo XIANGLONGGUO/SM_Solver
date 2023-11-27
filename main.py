852e50fd70e1cccf526b537e45731646cd125828
Guo Xianglong <2955839148@qq.com>
2023-11-27 17:49:34 +0800

    copy-1

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ if __name__=="__main__":
     soild_limitation=[]
     limitatation=[]
     Dict_disp={}
+    Lam = {}
+    Ke = {}
+    Me = {}
     while True:
         print('输入calculate来计算')
         print('输入add_node来添加节点')
@@ -23,6 +26,7 @@ if __name__=="__main__":
         print('输入remove_connect来删除连接')
         print('输入remove_point来删除节点间的节点')
         print('输入plot来绘图')
+        print('输入mat来输入材料参数')
         print('exit退出')
         #######################################################################
         #Edit the shape of structure
@@ -61,6 +65,79 @@ if __name__=="__main__":
             plot(node_list,connect_node)
         #######################################################################
         #create displcements and angle displacements
-        
+        elif imput=="mat" or imput=="m":
+            mode = int(input('请选择求解桁架还是刚架，桁架请输入0，刚架请输入1\n'))
+            Metal = input('请输入材料杨氏模量E，密度p，杆或梁的横截面积A，惯性矩Iz（杆输入0）,惯性矩Iy（杆输入0）,极惯性矩Ip（杆输入0）,剪切模量G（杆输入0）\n格式为E(GPa)，p(kg/m^3)，A(m^2)，Iz(m^4)，Iy(m^4)，Ip(m^4),G(GPa)\n').split(',')
+            Metal = list(map(float,Metal))
+            R = []
+            while True:
+                if mode == 0:
+                    if Metal[3] != 0 or Metal[4] != 0 or Metal[5] != 0 or Metal[6] != 0:
+                        print('Iz或Iy或Ip不等于0!!!!!')
+                        Metal = input('请输入材料杨氏模量E，密度p，杆或梁的横截面积A，惯性矩Iz（杆输入0）,惯性矩Iy（杆输入0）,极惯性矩Ip（杆输入0）,剪切模量G（杆输入0）\n格式为E(GPa)，p(kg/m^3)，A(m^2)，Iz(m^4)，Iy(m^4)，Ip(m^4),G(GPa)\n').split(',')
+                        Metal = list(map(float,Metal))
+                    else:
+                        print('设置完毕，桁架模式，材料参数如下',Metal)
+                        break
+                elif mode == 1:
+                    print('设置完毕，刚架模式，材料参数如下',Metal)
+                    break
+                else:
+                    print('模式选择错误，请重新选择')
+                    mode = int(input('请选择求解桁架还是刚架，桁架请输入0，刚架请输入1\n'))
+                    Metal = input('请输入材料杨氏模量E，密度p，杆或梁的横截面积A，惯性矩Iz（杆输入0）,惯性矩Iy（杆输入0）,极惯性矩Ip（杆输入0）,剪切模量G（杆输入0）\n格式为E(GPa)，p(kg/m^3)，A(m^2)，Iz(m^4)，Iy(m^4)，Ip(m^4),G(GPa)\n').split(',')
+                    Metal = list(map(float,Metal))
+        elif imput=='force' or imput=='f':
+            R=add_force(R,node_list,connect_node,mode)
+        elif imput=='calculate' or imput=='c':
+            
+
+            E,D,A,Iz,Iy,Ix,G = (Metal[0],Metal[1],Metal[2],Metal[3],Metal[4],Metal[5],Metal[6])
+            for i in len(connect_node):
+                node = connect_node[i]
+                op = node_list[node[0]]
+                ed = node_list[node[1]]
+                ele_array = np.array(op)-np.array(ed)
+                lenth = np.linalg.norm(ele_array)
+            
+                if mode == 0:
+                    Lam[i] = cal_lam(ele_array)
+                    k = np.zeros((6,6))
+                    k[0,0],k[3,3] = (1,1)
+                    k[3,0],k[0,3] = (-1,-1)
+                    m = np.matrix(2*np.eye(6,dtype=float))
+                    m[3:6,0:3],m[0:3,3:6] = (np.matrix(np.eye(3,dtype=float)),np.matrix(np.eye(3,dtype=float)))#一致质量矩阵
+                    Ke[i] = np.mat((E*10**9*A/lenth)*k)
+                    Me[i] = D*A*lenth*m/6
+                elif mode == 1:
+                    print(op,ed)
+                    enode = np.array(list(map(float,input('请输入任意主平面xy内点的坐标，格式为0,0,0\n').split(','))))-np.array(op)
+                    Lam[i] = cal_lam(ele_array,enode)
+                    k = np.matrix(np.zeros((12,12),dtype=float))
+                    a1,a2,a3,a4,a5,a6,a7,a8 = (E*A/lenth,12*E*Iz/lenth**3,6*E*Iz/lenth**2,12*E*Iy/lenth**3,-6*E*Iy/lenth**2,G*Ix/lenth,4*E*Iy/lenth,4*E*Iz/lenth)
+                    k[0,0],k[1,1],k[5,1],k[2,2],k[4,2],k[3,3],k[4,4],k[5,5] = (a1,a2,a3,a4,a5,a6,a7,a8)
+                    k[6:12,6:12] = k[0:6,0:6]
+                    k[11,7],k[10,8] = (-a3,-a5)
+                    k[6,0],k[7,1],k[11,1],k[8,2],k[10,2],k[9,3],k[8,4],k[10,4],k[7,5],k[11,5] = (-a1,-a2,a3,-a4,a5,-a6,-a5,-a5/3,-a3,a8/2)
+                    k += k.T - np.diag(k.diagonal())
+                    Ke[i] = np.matrix(k,dtype=float)
+                    m = np.matrix(np.zeros((12,12),dtype=float))
+                    m[0,0],m[1,1],m[5,1],m[2,2],m[4,2],m[3,3],m[4,4],m[5,5] = (140,156,22*lenth,156,-22*lenth,140*Ix/A,4*lenth**2,4*lenth**2)
+                    m[6:12,6:12] = m[0:6,0:6]
+                    m[11,7],m[10,8] = (-22*lenth,22*lenth)
+                    m[6,0],m[7,1],m[11,1],m[8,2],m[10,2],m[9,3],m[8,4],m[10,4],m[7,5],m[11,5] = (70,54,-13*lenth,54,13*lenth,70*Ix/A,-13*lenth,-3*lenth**2,13*lenth,-3*lenth**2)
+                    m += m.T - np.diag(m.diagonal())
+                    Me[i] = np.matrix(D*A*lenth*m/420,dtype=float)
+            u_and_fw_out = u_and_fw(mode,node_list,connect_node,dist_list,R,Lam,Ke,Me)
+            Uw = u_and_fw_out[0]#位移列向量
+            Fw = u_and_fw_out[1]#载荷列向量
+            K_an_M_out = cal_k_or_m(mode,node_list,connect_node,Lam,Ke,Me,Uw,Fw)
+            Kw = K_an_M_out[0]
+            Mw = K_an_M_out[1]
+            
+            Uw = cal(Fw,Kw,mode,node_list)
+            print(Uw)
+            s=50000000
+            draw_strain(node_list,connect_node,Uw,mode,s)
         else:
             print("输入错误")
--- utils.py
+++ utils.py
@@ -47,7 +47,7 @@ def add_connect_utils(node_list,connect_list):
     print('结束输入：end')
     while True:
         print('没有此力请输入0')
-        x = input("请输入节点的index index E I 前端点是否约束(0否，1简支，2固定) 后端点是否约束(0否，1简支，2固定) 左端点Fx Fy Fz Mx My Mz 右端点Fx Fy Fz Mx My Mz qx qy qz：")
+        x = input("请输入节点的index index")
         if x=="end":
             break
         if int(x.split(' ')[0])>len(node_list) or int(x.split(' ')[1])>len(node_list):
@@ -69,7 +69,7 @@ def add_point_utils(node_list,connect_list,dist_list):
     I=0
     print('结束输入：end')
     while True:
-        x = input("请输入节点的连接序号 起始点距离占比 是否固定(0否，1简支，2固定) 端点Fx Fy Fz Mx My Mz：")
+        x = input("请输入节点的连接序号 起始点距离占比：")
         if x=="end":
             print('node_list')
             for i in range(len(node_list)):
@@ -189,4 +189,153 @@ def plot(node_list,connect_list):
 
 
 
+def add_force(R,node_list,connect_list,mode):
+#输入外载荷，单元内的外力平均分配到两结点，
 
+    print('载荷施加在结点上，用列表表示其性质：[作用结点号，类型(力为0，力矩为1)，x方向分量，y方向分量，z方向分量]\n节点编号与结构图如下')
+    plot(node_list,connect_list)
+    force_input = input('请输入\n作用结点号，类型(力为0，力矩为1,杆只能输入力)，x方向分量，y方向分量，z方向分量\n输入完毕请输入0结束此步骤\n').split(',')
+    force_input = list(map(float,force_input))
+    while True:
+        if force_input == [0]:
+            print('输入完毕，外载荷如下')
+            for i in range(0,len(R)):
+                F = R[i]
+                print('结点'+str(F[0]),'类型'+str(F[1]),'x方向分量'+str(F[2]),'y方向分量'+str(F[3]),'z方向分量'+str(F[4]))
+            break
+        else:
+            if len(force_input) != 5:
+                print('参数不全!!!!!')
+                force_input = input('请输入\n作用结点号，类型(力为0，力矩为1)，x方向分量，y方向分量，z方向分量\n输入完毕请输入0结束此步骤\n').split(',')
+                force_input = list(map(float,force_input))
+            elif mode == 0 and force_input[1] != 0:
+                print('桁架模式下只能输入力!!!!!')
+                force_input = input('请输入\n作用结点号，类型(力为0，力矩为1)，x方向分量，y方向分量，z方向分量\n输入完毕请输入0结束此步骤\n').split(',')
+                force_input = list(map(float,force_input))
+            else:
+                R.append(force_input)
+                print('输入成功，请输入下一载荷')
+                force_input = input('请输入\n作用结点号，类型(力为0，力矩为1)，x方向分量，y方向分量，z方向分量\n输入完毕请输入0结束此步骤\n').split(',')
+                force_input = list(map(float,force_input))
+#单元矩阵计算
+def cal_lam(ary,mode,ex_node = 0):
+    ele_x = ary/np.linalg.norm(ary)
+    if mode == 0:
+        if ele_x[0]==0 and ele_x[1]==0:
+            ele_y = np.array([1,0,0])
+            ele_z = np.array([0,1,0])
+        else:
+            ele_y = np.array([-ele_x[1],ele_x[0],0])
+            cross = np.cross(ele_x, ele_y)
+            ele_z = cross/np.linalg.norm(cross)
+
+        cos_xx,cos_xy,cos_xz,cos_yx,cos_yy,cos_yz,cos_zx,cos_zy,cos_zz = (ele_x[0],ele_x[1],ele_x[2],ele_y[0],ele_y[1],ele_y[2],ele_z[0],ele_z[1],ele_z[2])
+        lam = np.matrix([[cos_xx,cos_yx,cos_zx],[cos_xy,cos_yy,cos_zy],[cos_xz,cos_yz,cos_zz]])
+        return lam
+    elif mode == 1:
+        lx,mx,nx = (ele_x[0],ele_x[1],ele_x[2])
+        enx = ex_node/np.linalg.norm(ex_node)
+        p1,p2,p3 = (enx[0],enx[1],enx[2])
+        s = np.sqrt((mx*p3-nx*p2)**2+(nx*p1-lx*p3)**2+(lx*p2-mx*p1)**2)
+        lmn = lx*p1+mx*p2+nx*p3
+        ly,my,ny,lz,mz,nz = ((p1-lx*lmn)/s,(p2-mx*lmn)/s,(p3-nx*lmn)/s,(mx*p3-nx*p2)/s,(nx*p1-lx*p3)/s,(lx*p2-mx*p1)/s)
+        lam = np.matrix([[lx,ly,lz],[mx,my,mz],[nx,ny,nz]])
+        return lam
+
+def cal_k_or_m(m,Node,Rod,Lam,Ke,Me):
+    kw = np.matrix(np.zeros((3*(m+1)*len(Node),3*(m+1)*len(Node)),dtype=float))
+    mw = np.matrix(np.zeros((3*(m+1)*len(Node),3*(m+1)*len(Node)),dtype=float))
+    t0 = np.matrix(np.zeros((3,3),dtype=float))
+    for i in len(Rod):
+        start_num = Rod[i][0]
+        end_num = Rod[i][1]
+        if m==0:
+            te = np.vstack((np.hstack((Lam[i],t0)),np.hstack((t0,Lam[i]))))
+        elif m==1:
+            te = np.vstack((np.hstack((Lam[i],t0,t0,t0)),np.hstack((t0,Lam[i],t0,t0)),np.hstack((t0,t0,Lam[i],t0)),np.hstack((t0,t0,t0,Lam[i]))))
+        ke = np.dot(np.dot(te,Ke[i]),te.T)
+        me = np.dot(np.dot(te,Me[i]),te.T)
+        start1,start2,end1,end2 = (int(3*(m+1)*start_num),int(3*(m+1)*(start_num+1)),int(3*(m+1)*end_num),int(3*(m+1)*(end_num+1)))
+        n1,n2 = (int(3*(m+1)),int(6*(m+1)))
+        kw[start1:start2,start1:start2] += ke[0:n1,0:n1]
+        kw[start1:start2,end1:end2] += ke[0:n1,n1:n2]
+        kw[end1:end2,start1:start2] += ke[n1:n2,0:n1]
+        kw[end1:end2,end1:end2] += ke[n1:n2,n1:n2]
+        mw[start1:start2,start1:start2] += me[0:n1,0:n1]
+        mw[start1:start2,end1:end2] += me[0:n1,n1:n2]
+        mw[end1:end2,start1:start2] += me[n1:n2,0:n1]
+        mw[end1:end2,end1:end2] += me[n1:n2,n1:n2]
+    return [kw,mw]
+def u_and_fw(m,Node,R,Rod,Lam,Ke,Me):
+    u = np.zeros(((m+1)*3*len(Node),1),dtype=float)
+    fw = np.zeros(((m+1)*3*len(Node),1),dtype=float)
+    for i in range(0,len(R)):
+        force_inf = R[i]
+        for j in range(0,3):
+            fw[(int(3*force_inf[0]+3*force_inf[1]+j),0)] += force_inf[j+2]
+    return [u,fw]
+def cal(Fw,Kw,mode,Node):
+    #求解静力位移
+    #输入位移约束
+    Us = [[0,0,0],[0,1,0],[1,0,0],[1,1,0]]#输入约束，元素为列表，每个代表一项约束，其中第一项为结点号，第二项为位移分量编号，第三项为位移值
+    #处理刚度矩阵与载荷列向量
+    for i in range(0,len(Us)):
+        us = Us[i]
+        Fw[int(3*(mode+1)*us[0]+us[1]),0] = us[2]
+        Kw[int(3*(mode+1)*us[0]+us[1]),:] = 0
+        Kw[:,int(3*(mode+1)*us[0]+us[1])] = 0
+        Kw[int(3*(mode+1)*us[0]+us[1]),int(3*(mode+1)*us[0]+us[1])] = 1
+
+    Z = []
+    for i in len(Node):
+        Z.append(float(Node[i][-1]))
+    if Z==[0.0]*len(Node):
+        for i in Node.keys():
+            Fw[int(3*(mode+1)*i+2),0] = 0
+            Kw[int(3*(mode+1)*i+2),:] = 0
+            Kw[:,int(3*(mode+1)*i+2)] = 0
+            Kw[int(3*(mode+1)*i+2),int(3*(mode+1)*i+2)] = 1
+            if mode==1:
+                Fw[int(3*(mode+1)*i+4),0] = 0
+                Kw[int(3*(mode+1)*i+4),:] = 0
+                Kw[:,int(3*(mode+1)*i+4)] = 0
+                Kw[int(3*(mode+1)*i+4),int(3*(mode+1)*i+4)] = 1
+                Fw[int(3*(mode+1)*i+5),0] = 0
+                Kw[int(3*(mode+1)*i+5),:] = 0
+                Kw[:,int(3*(mode+1)*i+5)] = 0
+                Kw[int(3*(mode+1)*i+5),int(3*(mode+1)*i+5)] = 1
+    return np.dot(Kw.I,Fw)
+
+# Drawing strains with scaling
+def draw_strain(dic, rod, U,mode, scale_factor=1000):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+
+    # Original structure
+    for i in len(rod):
+        rod_node = rod[i]
+        node_coord1 = dic[rod_node[0]]
+        node_coord2 = dic[rod_node[1]]
+        x, y, z = [node_coord1[0], node_coord2[0]], [node_coord1[1], node_coord2[1]], [node_coord1[2], node_coord2[2]]
+        ax.scatter(x, y, z, c='red', s=10)
+        ax.plot(x, y, z, color='black')
+
+    # Deformed structure with scaling
+    for i in len(rod):
+        rod_node = rod[i]
+        node_coord1 = dic[rod_node[0]]
+        node_coord2 = dic[rod_node[1]]
+        x = [node_coord1[0] + scale_factor * U[int(3 * (mode + 1) * rod_node[0])], 
+             node_coord2[0] + scale_factor * U[int(3 * (mode + 1) * rod_node[1])]]
+        y = [node_coord1[1] + scale_factor * U[int(3 * (mode + 1) * rod_node[0] + 1)], 
+             node_coord2[1] + scale_factor * U[int(3 * (mode + 1) * rod_node[1] + 1)]]
+        z = [node_coord1[2] + scale_factor * U[int(3 * (mode + 1) * rod_node[0] + 2)], 
+             node_coord2[2] + scale_factor * U[int(3 * (mode + 1) * rod_node[1] + 2)]]
+
+        # Flatten the arrays before passing to scatter and plot functions
+        x, y, z = np.array(x).flatten(), np.array(y).flatten(), np.array(z).flatten()
+
+        ax.scatter(x, y, z, c='blue', s=10)
+        ax.plot(x, y, z, color='black')
+
+    plt.show()

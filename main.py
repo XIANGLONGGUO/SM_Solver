406a10fedee182d4ba94fcf6aae7023a120dc82f
Guo Xianglong <2955839148@qq.com>
2023-11-30 16:25:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from calculate import *
 if __name__=="__main__":
     print('Welcome to SM_Solver')
     print('author:GUOxianglong')
-    node_list=[]
-    connect_node=[]
+    node_list=[[0,0,0],[0,1,0],[1,1,0],[1,0,0]]####
+    connect_node=[[0,1],[1,2],[2,3],[3,0],[0,2],[1,3]]####
     dist_list=[]
     soild_limitation=[]
     l=[]
@@ -93,9 +93,11 @@ if __name__=="__main__":
                     Metal = list(map(float,Metal))
         elif imput=='force' or imput=='f':
             R=add_force(R,node_list,connect_node,mode)
-        elif imput=='calculate' or imput=='c':
+        elif imput=='l':
             l=get_l(l)
             Us=convert(l)
+        elif imput=='calculate' or imput=='c':
+
             E,D,A,Iz,Iy,Ix,G = (Metal[0],Metal[1],Metal[2],Metal[3],Metal[4],Metal[5],Metal[6])
             for i in range(len(connect_node)):
                 node = connect_node[i]
@@ -144,6 +146,49 @@ if __name__=="__main__":
             draw_strain(node_list,connect_node,Uw,mode,s)
         
         elif imput=='hz':
+
+            E,D,A,Iz,Iy,Ix,G = (Metal[0],Metal[1],Metal[2],Metal[3],Metal[4],Metal[5],Metal[6])
+            for i in range(len(connect_node)):
+                node = connect_node[i]
+                op = node_list[node[0]]
+                ed = node_list[node[1]]
+                ele_array = np.array(op)-np.array(ed)
+                lenth = np.linalg.norm(ele_array)
+            
+                if mode == 0:
+                    print('衔架')
+                    Lam[i] = cal_lam(ele_array,mode)
+                    k = np.matrix([[1,-1],[-1,1]],dtype=float)
+                    m = np.matrix([[2,1],[1,2]],dtype=float)#一致质量矩阵
+                    Ke[i] = np.mat((E*10**9*A/lenth)*k)
+                    Me[i] = D*A*lenth*m/6
+                elif mode == 1:
+                    print('钢架')
+                    print(op,ed)
+                    enode = np.array(list(map(float,input('请输入任意主平面xy内点的坐标，格式为0,0,0\n').split(','))))-np.array(op)
+                    Lam[i] = cal_lam(ele_array,mode,enode)
+                    k = np.matrix(np.zeros((12,12),dtype=float))
+                    a1,a2,a3,a4,a5,a6,a7,a8 = (E*A/lenth,12*E*Iz/lenth**3,6*E*Iz/lenth**2,12*E*Iy/lenth**3,-6*E*Iy/lenth**2,G*Ix/lenth,4*E*Iy/lenth,4*E*Iz/lenth)
+                    k[0,0],k[1,1],k[5,1],k[2,2],k[4,2],k[3,3],k[4,4],k[5,5] = (a1,a2,a3,a4,a5,a6,a7,a8)
+                    k[6:12,6:12] = k[0:6,0:6]
+                    k[11,7],k[10,8] = (-a3,-a5)
+                    k[6,0],k[7,1],k[11,1],k[8,2],k[10,2],k[9,3],k[8,4],k[10,4],k[7,5],k[11,5] = (-a1,-a2,a3,-a4,a5,-a6,-a5,-a5/3,-a3,a8/2)
+                    k += k.T - np.diag(k.diagonal())
+                    Ke[i] = np.matrix(k,dtype=float)
+                    m = np.matrix(np.zeros((12,12),dtype=float))
+                    m[0,0],m[1,1],m[5,1],m[2,2],m[4,2],m[3,3],m[4,4],m[5,5] = (140,156,22*lenth,156,-22*lenth,140*Ix/A,4*lenth**2,4*lenth**2)
+                    m[6:12,6:12] = m[0:6,0:6]
+                    m[11,7],m[10,8] = (-22*lenth,22*lenth)
+                    m[6,0],m[7,1],m[11,1],m[8,2],m[10,2],m[9,3],m[8,4],m[10,4],m[7,5],m[11,5] = (70,54,-13*lenth,54,13*lenth,70*Ix/A,-13*lenth,-3*lenth**2,13*lenth,-3*lenth**2)
+                    m += m.T - np.diag(m.diagonal())
+                    Me[i] = np.matrix(D*A*lenth*m/420,dtype=float)
+            u_and_fw_out = u_and_fw(mode,node_list,R,connect_node,Lam,Ke,Me)
+            Uw = u_and_fw_out[0]#位移列向量
+            Fw = u_and_fw_out[1]#载荷列向量
+            K_an_M_out = cal_k_or_m(mode,node_list,connect_node,Lam,Ke,Me)
+            Kw = K_an_M_out[0]
+            Mw = K_an_M_out[1]
+            
             Uss=[None]*len(node_list)
             for i in range(len(node_list)):
                 Uss[i]=[0,0,0]
